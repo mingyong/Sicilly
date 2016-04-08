d267287fc5fc963e3426858ed9ef25a28aa337ec
shaohuihui <961492943@qq.com>
2016-04-08 14:08:20 +0800

    修复登录页面statusBar

--- app/src/main/java/xyz/shaohui/sicilly/ui/activities/LoginActivity.java
+++ app/src/main/java/xyz/shaohui/sicilly/ui/activities/LoginActivity.java
@@ -2,10 +2,13 @@ package xyz.shaohui.sicilly.ui.activities;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -37,6 +40,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window window = getWindow();
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+
         ButterKnife.bind(this);
 
         initEditText();
