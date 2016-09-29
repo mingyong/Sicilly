c6d73a9f12be39bcfb521ed3bbcc4eac31649ee9
shaohui10086 <961492943@qq.com>
2016-09-30 01:06:19 +0800

    新建status Dialog

---
+++ app/src/main/java/xyz/shaohui/sicilly/views/create_status/CreateStatusDialog.java
@@ -0,0 +1,21 @@
+package xyz.shaohui.sicilly.views.create_status;
+
+import android.view.View;
+import xyz.shaohui.sicilly.R;
+import xyz.shaohui.sicilly.base.BaseDialogFragment;
+
+/**
+ * Created by shaohui on 16/9/30.
+ */
+
+public class CreateStatusDialog extends BaseDialogFragment {
+    @Override
+    public int layoutRes() {
+        return R.layout.dialog_create_status;
+    }
+
+    @Override
+    public void bindView(View view) {
+
+    }
+}
